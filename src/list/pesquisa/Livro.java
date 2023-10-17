8ec3b38db2e3b3e763942b9f6111c921d0952bd2
gabrieltorquat0 <gabrielviniciust@hotmail.com>
2023-10-17 17:33:02 -0300

    Resolvendo exercícios CatalogoLivros e OrdenacaoPessoa
    Signed-off-by: gabrieltorquat0 <gabrielviniciust@hotmail.com>

---
+++ src/list/pesquisa/Livro.java
@@ -0,0 +1,50 @@
+package list.pesquisa;
+
+public class Livro {
+    //atributos
+
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        CatalogoLivros catalogoLivros = new CatalogoLivros();
+
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 1", 2020);
+        catalogoLivros.adicionarLivro("Livro 1", "Autor 2", 2021);
+        catalogoLivros.adicionarLivro("Livro 2", "Autor 2", 2022);
+        catalogoLivros.adicionarLivro("Livro 3", "Autor 3", 2023);
+        catalogoLivros.adicionarLivro("Livro 4", "Autor 4", 1994);
+
+        System.out.println(catalogoLivros.pesquisarPorAutor("Autor 4"));
+        System.out.println(catalogoLivros.pesquisarPorIntervaloAnos(2020,2021));
+        System.out.println(catalogoLivros.pesquisarPorTitulo("Livro 1"));
+    }
+}
